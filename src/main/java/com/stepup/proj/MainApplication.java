7318b790021af749653babb3d8c12783785f2ebc
Kazym Aslanov <kazim.5006@gmail.com>
2024-08-31 12:14:59 +0300

    initial commit

---
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -0,0 +1,9 @@
+package com.stepup.proj;
+
+import java.sql.SQLOutput;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
