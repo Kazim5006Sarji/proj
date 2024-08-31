3bbfddda67f2a2f76d7f3d22980b32adf24ac004
Kazym Aslanov <kazim.5006@gmail.com>
2024-08-31 12:25:13 +0300

    upd

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("11111");
     }
 }
